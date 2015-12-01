85d8f402cb2fcaebf20454cc2a896cd0f8300772
resteve <resteve@zikzakmedia.com>
2015-12-01 11:08:30 +0100

    Tests

--- tests/test_galatea_photoalbum.py
+++ tests/test_galatea_photoalbum.py
@@ -1,29 +1,18 @@
-#!/usr/bin/env python
-# This file is part galatea_photoalbum module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the galatea_photoalbum module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class GalateaPhotoAlbumTestCase(unittest.TestCase):
-    'Test Galatea PhotoAlbum module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('galatea_photoalbum')
-
-    def test0005views(self):
-        'Test views'
-        test_view('galatea_photoalbum')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class GalateaPhotoalbumTestCase(ModuleTestCase):
+    'Test Galatea Photoalbum module'
+    module = 'galatea_photoalbum'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-        GalateaPhotoAlbumTestCase))
+        GalateaPhotoalbumTestCase))
     return suite
